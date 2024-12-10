f310bfed9805678004ced165a5041b14b30ca221
ɪᴍ-ɴᴏᴛʏᴄᴏᴅᴇʀ <130166544+im-notycoder@users.noreply.github.com>
2024-12-10 14:20:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 
 API_ID = int(getenv("API_ID", "21432914"))
-API_HASH = getenv("API_HASH", " 4d65415fcce04e8076acee5b6688c1b1")
+API_HASH = getenv("API_HASH", "4d65415fcce04e8076acee5b6688c1b1")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7874236359:AAGEMmYoMStr2EKNWtAhXjnvPz47TMaKWZI")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
