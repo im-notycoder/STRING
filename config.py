d896dce40d79283a9665cba61134ae869088a2ae
ɪᴍ-ɴᴏᴛʏᴄᴏᴅᴇʀ <130166544+im-notycoder@users.noreply.github.com>
2024-12-10 14:19:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "21432914"))
 API_HASH = getenv("API_HASH", " 4d65415fcce04e8076acee5b6688c1b1")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6466585201:AAE7cIgC8zlzt1aEK1UXQYcsIz2YrR5UvH8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7874236359:AAGEMmYoMStr2EKNWtAhXjnvPz47TMaKWZI")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 5617239137))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Nxt_Bots")
